d00a787f18cc3e4ce01acc41bb34f59a81dceb3c
ErebusAres <erebusares@gmail.com>
2026-06-22 16:34:14 -0500

    Update bungie-config.js

--- data/bungie-config.js
+++ data/bungie-config.js
@@ -1,5 +1,5 @@
 window.D2_BUNGIE_CONFIG = {
-  apiKey: "",
+  apiKey: "939b3c127bfc4ab5aa4e68093becbf30",
   authUrl: "https://www.bungie.net/en/OAuth/Authorize",
   clientId: "53180",
   tokenUrl: "https://www.bungie.net/Platform/App/OAuth/Token/",
